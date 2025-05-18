29f8bdaab597cedbc2f782a1e9009dc83aa158c2
Ricardo de Campos Cavanha <ricardocavanha@live.com>
2025-05-18 15:12:38 -0400

    tasks 3 ok

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,14 +20,36 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
     // get the ISBN
     const isbn=req.params.isbn;
-    // filter for that isbn
+    // filter for that isbn and send it
     res.send(JSON.stringify(books[isbn]));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet yo be implemented"});
+    // get the author from the request
+    const author=req.params.author;
+    // initialize the found marker
+    let marker=false;
+    // initialize the aBooks array
+    let aBooks=[];
+    // map the book entries
+    for (let [key, value] of Object.entries(books)) {
+        // if the book entry matches the 
+        if (books[key].author===author) {
+            // reset the marker
+            marker=true;
+            // append to the aBooks array
+            aBooks.push(books[key]);
+        }
+    };
+    // if the author was found
+    if (marker) {
+        // send the books info
+        res.send(JSON.stringify(aBooks));
+    } else {
+        //Write your code here
+        return res.status(404).json({message: "Author not found"});
+    }
 });
 
 // Get all books based on title
